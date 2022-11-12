be027e718570b6e18d234652abcd9bdddebee039
dev.bashar <dev.basharabdullah@outlook.com>
2022-11-12 11:13:45 +0300

    Finish add banner

--- pages/addBanner/index.js
+++ pages/addBanner/index.js
@@ -2,7 +2,7 @@ import Main from "@/components/layout/main";
 import { Button, message } from "antd";
 import axios from "axios";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 import useFetch from "utils/useFetch";
@@ -11,17 +11,11 @@ function AddBanner() {
     const [imageName, setImageName] = useState("");
     const [cookies, setCookies, clearCookies] = useCookies(["user"]);
     const [banners, setBanners] = useState([]);
+    const [response, setResponse] = useState("");
+    const [, updateState] = useState();
 
-    useEffect(async () => {
-        await axios
-            .get(process.env.NEXT_PUBLIC_HOST_API + "api/ListBanner1", {})
-            .then((response) => {
-                if (response.data.description.length > 0) {
-                    console.log("bann", response.data.description);
-                    setBanners(response.data.description);
-                }
-            })
-            .catch((err) => console.error(err));
+    useEffect(() => {
+        getDataList();
     }, []);
 
     const {
@@ -35,6 +29,17 @@ function AddBanner() {
         {},
         false
     );
+    const {
+        data: editData = {},
+        error: editError,
+        loading: editLoading,
+        executeFetch: editExecuteFetch,
+    } = useFetch(
+        `${process.env.NEXT_PUBLIC_HOST_API}api/EditBanner`,
+        "post",
+        {},
+        false
+    );
 
     const handleAddBanner = async () => {
         let formData = new FormData();
@@ -43,13 +48,58 @@ function AddBanner() {
         addExecuteFetch(formData, true);
     };
 
-    const success = () => {
-        message.success("The banner image is added");
+    const handleEditBanner = (id) => {
+        console.log(id);
+        let formData = new FormData();
+        formData.append("Banner", "05-min-1-1024x683.jpg");
+        formData.append("BannerNumber", 0);
+        formData.append("id", id);
+        editExecuteFetch(formData, true);
+    };
+
+    console.log(editData);
+    // Handle delete banner
+    const handleDelete = async (id) => {
+        console.log(id);
+        await axios
+            .post(
+                `${process.env.NEXT_PUBLIC_HOST_API}api/Deletebanner`,
+                {
+                    id: id,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${cookies?.user?.token}`,
+                    },
+                }
+            )
+            .then((res) => {
+                success("The banner is deleted!");
+                getDataList();
+                console.log(res.data);
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const getDataList = async () => {
+        await axios
+            .get(process.env.NEXT_PUBLIC_HOST_API + "api/ListBanner1", {})
+            .then((response) => {
+                if (response.data.description.length > 0) {
+                    console.log("bann", response.data.description);
+                    setBanners(response.data.description);
+                }
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const success = (_message) => {
+        message.success(_message);
     };
     return (
         <Main>
             <div className="banner__container">
-                <div className="add-banner banner">
+                <div className="add-banner ">
                     <div className="add">
                         <h2>Add banner</h2>
                         <h4>File Upload Here</h4>
@@ -86,16 +136,62 @@ function AddBanner() {
                     </div>
                 </div>
 
+                <h2>Previus banners</h2>
                 <div className="add-banner banner-list">
                     {banners?.map((item, index) => {
                         return (
-                            <Image
-                                height={100}
-                                width={100}
-                                src={`https://dashcommerce.click68.com/${item.banner}`}
-                                key={index}
-                                layout="fixed"
-                            />
+                            <div className="image-container">
+                                <span className="grad"></span>
+                                <Image
+                                    height={150}
+                                    width={300}
+                                    src={`https://dashcommerce.click68.com/${item?.banner}`}
+                                    key={index}
+                                    layout="fixed"
+                                />
+                                <div className="actions">
+                                    <div
+                                        className="action"
+                                        onClick={() => handleDelete(item?.id)}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="16"
+                                            height="16"
+                                            fill="currentColor"
+                                            class="bi bi-trash"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <div
+                                        className="action"
+                                        onClick={() =>
+                                            handleEditBanner(item?.id)
+                                        }
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="16"
+                                            height="16"
+                                            fill="currentColor"
+                                            class="bi bi-pencil-square"
+                                            viewBox="0 0 16 16"
+                                        >
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                                            />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
                         );
                     })}
                 </div>
